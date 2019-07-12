b0ee1d8156f50c64108299eefc1c87d3394f8112
Mitina Rita <rita.mitina.00@mail.ru>
2019-07-13 01:50:30 +0300

    completed

--- assets/scripts/onePageScroll.js
+++ assets/scripts/onePageScroll.js
@@ -1,116 +1,121 @@
 
 
-// const sections = $(".section");
-// const display = $(".maincontent");
-// let inScroll = false;
 
-// const setActiveItem = itemEq =>{
-//   $(".pagination__item").eq(itemEq)
-//   .addClass('pagination__item--active')
-//   .siblings()
-//   .removeClass('pagination__item--active');
-// }
+let inScroll = false;
 
-
-// //определение секции
-// const performTransition = sectionEq => {
-//   const position = `${sectionEq * -100}%`;
-//   if (inScroll == false){
-//     inScroll = true;
-//     $(".maincontent").css({
-//       transform: `translateY(${position})`
-//     });
-  
-//     $(".section").eq(sectionEq)
-//     .addClass('active')
-//     .siblings()
-//     .removeClass('active');
-
-//     setTimeout(() => {
-//       inScroll = false;
-//       setActiveItem(sectionEq);
-//     }, 1000+300);  //энерция мыши
-//   }
-// };
-
-// $('[data-scroll-to]').on('click', e=>{
-//   e.preventDefault;
-//   console.log("333");
-//   const target = $(e.currentTarget).attr('data-scroll-to');
-//   performTransition(target);
-// });
-
-
-
-// // скролл секций
-// const scrollToSection = direction => {
-//   const activeSection = $(".section").filter('.active');
-//   const prevSection = activeSection.prev();
-//   const nextSection = activeSection.next();
-
-//   if (direction == "up" && prevSection.length){
-//     performTransition(prevSection.index());
-//   }
-
-//   if (direction == "down" && nextSection.length){
-//     performTransition(nextSection.index());
-//   }
-// }
-
-
-// //обработчик скролла
-// $(document).on("wheel", e => {
-
-//   const deltaY = e.originalEvent.deltaY;
-//   if (deltaY > 0 ){
-//     console.log("Вниз");
-//     scrollToSection("down");
-//   }
-//   if (deltaY < 0 ){
-//     //вверх
-//     scrollToSection("up");
-
-//   }
-// });
-
-
-
-
-
-
-
-var touchstartX = 0;
-var touchstartY = 0;
-var touchendX = 0;
-var touchendY = 0;
-
-
-document.addEventListener('touchstart', function(event) {
-    touchstartX = event.screenX;
-    touchstartY = event.screenY;
-}, false);
-
-document.addEventListener('touchend', function(event) {
-    touchendX = event.screenX;
-    touchendY = event.screenY;
-    handleGesure();
-}, false); 
-
-function handleGesure() {
-    var swiped = 'swiped: ';
-    if (touchendX < touchstartX) {
-        alert(swiped + 'left!');
-    }
-    if (touchendX > touchstartX) {
-        alert(swiped + 'right!');
-    }
-    if (touchendY < touchstartY) {
-        alert(swiped + 'down!');
-    }
-    if (touchendY > touchstartY) {
-        alert(swiped + 'left!');
-    }
-    if (touchendY == touchstartY) {
-        alert('tap!');
-    }
+const setActiveItem = itemEq =>{
+  $(".pagination__item").eq(itemEq)
+  .addClass('pagination__item--active')
+  .siblings()
+  .removeClass('pagination__item--active');
 }
+
+
+//определение секции
+const performTransition = sectionEq => {
+  const position = `${sectionEq * -100}%`;
+  if (inScroll == false){
+    inScroll = true;
+    $(".maincontent").css({
+      transform: `translateY(${position})`
+    });
+  
+    $(".section").eq(sectionEq)
+    .addClass('active')
+    .siblings()
+    .removeClass('active');
+    switch(sectionEq){
+      case 0:
+        $(".down").css({
+          "display":"block"
+        });
+        break;
+      case 1:
+        $(".down").css({
+          "display":"block"
+        });
+
+        break;
+      case 2:
+      $(".down").css({
+        "display":"none"
+      });
+    }
+    setTimeout(() => {
+      inScroll = false;
+      setActiveItem(sectionEq);
+
+    }, 1000+300);  //энерция мыши
+  }
+};
+
+
+
+
+// скролл секций
+const scrollToSection = direction => {
+  
+  const activeSection = $(".section").filter('.active');
+  const prevSection = activeSection.prev();
+  const nextSection = activeSection.next();
+  var activeAnimate = $(".pathogenesis__content__piece");
+
+  if (direction == "up" && prevSection.length){
+    performTransition(prevSection.index());
+
+    $(".pathogenesis__content__piece").removeClass('pathogenesis__content__piece--animate-down')
+    .addClass('pathogenesis__content__piece--animate-up').removeClass('pathogenesis__content__piece--animate-down');
+  
+  }
+
+  if (direction == "down" && nextSection.length){
+   
+    performTransition(nextSection.index());
+    
+    $(".pathogenesis__content__piece").removeClass('pathogenesis__content__piece--animate-up')
+    .addClass('pathogenesis__content__piece--animate-down').removeClass('pathogenesis__content__piece--animate-up');
+  //  if($(".pathogenesis__content__pieces .pathogenesis__content__piece:not(.pathogenesis__content__piece--animate-down)")
+  //              .addClass('pathogenesis__content__piece--animate-down');
+    
+  
+  }
+}
+
+
+//обработчик скролла
+$(document).on("wheel", e => {
+
+  const deltaY = e.originalEvent.deltaY;
+  if (deltaY > 0 ){
+    
+    scrollToSection("down");
+ 
+  
+  }
+  if (deltaY < 0 ){
+    // $(".pathogenesis__content__piece").css({"margin-top":"+=30px"});
+    scrollToSection("up");
+  
+    // $(".pathogenesis__content__piece").css({"margin-top":"0px"});
+  }
+});
+
+
+$(document).swipe( {
+
+  swipe:function(
+    
+    event,
+    direction,
+    distance,
+    duration,
+    fingerCount,
+    fingerData
+         )
+         
+          {
+    const scrollDirection = direction == "down" ? "up" : "down"
+    scrollToSection(scrollDirection);
+   
+  }
+});
